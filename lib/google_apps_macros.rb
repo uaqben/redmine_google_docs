7dee38525cbe81007da68b16f596b2d99a80295c
Tomasz Giełda <tomek@ebimedia.pl>
2015-04-24 08:49:28 +0200

    Added linking to original documents.

--- lib/google_apps_macros.rb
+++ lib/google_apps_macros.rb
@@ -9,7 +9,7 @@ module GoogleAppsMacros
   class DocumentMacros
     def self.get_doc(obj,args)
       if args[0].start_with?("https://docs.google.com/")
-        out = "<iframe src='#{args[0]}' width='800' height='400'></iframe>"
+        out = "<iframe src='#{args[0]}' width='800' height='400'></iframe></br><a href='#{args[0]}'>Go to original Google Document</a>"
       else
         raise "It is not Google Apps document..."
       end
