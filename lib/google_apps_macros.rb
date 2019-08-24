37bd37a2cf45b063de0564a6336e01939fa88a31
ben <11063165+uaqben@users.noreply.github.com>
2019-08-24 17:36:37 +0200

    Increase size, rename button & prepare css styling

--- lib/google_apps_macros.rb
+++ lib/google_apps_macros.rb
@@ -8,8 +8,8 @@ module GoogleAppsMacros
 
   class DocumentMacros
     def self.get_doc(obj,args)
-      wid = 800
-      hei = 400
+      wid = 100%
+      hei = 600
       if args.length > 1
         if args.length != 3
           raise "You will need two dimensions of iframe as arguments."
@@ -23,7 +23,7 @@ module GoogleAppsMacros
         end
       end
       if args[0].start_with?("https://docs.google.com/")
-        out = "<iframe src='#{args[0]}' width='#{wid}' height='#{hei}'></iframe></br><a href='#{args[0]}' target='_blank'>Go to original Google Document</a>"
+        out = "<iframe src='#{args[0]}' width='#{wid}' height='#{hei}'></iframe></br><a id='gdoc-btn' href='#{args[0]}' target='_blank'>Open in Google Doc</a>"
       else
         raise "It is not Google Apps document..."
       end
